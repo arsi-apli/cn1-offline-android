6eba93e91e4d5b352ea97ef06293cdd60841f392
arsi arsi <arsi arsi@arsi.sk>
2017-08-21 17:08:34 +0200

    v1 - cn1 demos compiled without problems

--- src/main/java/sk/arsi/cn1/offline/android/explorer/AndroidExplorerFactory.java
+++ src/main/java/sk/arsi/cn1/offline/android/explorer/AndroidExplorerFactory.java
@@ -6,13 +6,28 @@
 package sk.arsi.cn1.offline.android.explorer;
 
 import java.awt.Image;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
 import javax.swing.Action;
+import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import org.apache.commons.io.IOUtils;
 import org.netbeans.api.annotations.common.StaticResource;
 import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectManager;
+import org.netbeans.gradle.project.NbGradleProjectFactory;
+import org.netbeans.spi.project.ProjectState;
+import org.netbeans.spi.project.ui.LogicalViewProvider;
 import org.netbeans.spi.project.ui.support.NodeFactory;
 import org.netbeans.spi.project.ui.support.NodeList;
 import org.openide.filesystems.FileObject;
@@ -24,6 +39,7 @@ import org.openide.nodes.Node;
 import org.openide.util.Exceptions;
 import org.openide.util.ImageUtilities;
 import org.openide.util.Utilities;
+import org.openide.util.WeakListeners;
 
 /**
  *
@@ -36,18 +52,17 @@ public class AndroidExplorerFactory implements NodeFactory {
     private static final String ANDROID_ICON_RES = "sk/arsi/cn1/offline/16_android.gif";
     public static final Image ANDROID_ICON = ImageUtilities.loadImage(ANDROID_ICON_RES);
 
-    public AndroidExplorerFactory() {
-        System.out.println("sk.arsi.cn1.offline.android.explorer.AndroidExplorerFactory.<init>()");
-    }
-
     @Override
     public NodeList<?> createNodes(Project p) {
         return new AndroidFolderList(p);
     }
 
+    private final List<ChangeListener> listeners = new ArrayList<>();
+
     private class AndroidFolderList implements NodeList<FileObject> {
 
         private final Project project;
+        private FileObject androidDir;
 
         public AndroidFolderList(Project project) {
             this.project = project;
@@ -60,7 +75,8 @@ public class AndroidExplorerFactory implements NodeFactory {
             FileObject cn1PropertiesFile = projectDir.getFileObject("codenameone_settings.properties");
             if (cn1PropertiesFile != null) {
                 try {
-                    FileObject androidDir = FileUtil.createFolder(projectDir, "android");
+                    androidDir = FileUtil.createFolder(projectDir, "android");
+
                     fileObjects.add(androidDir);
                 } catch (IOException ex) {
                     Exceptions.printStackTrace(ex);
@@ -71,17 +87,19 @@ public class AndroidExplorerFactory implements NodeFactory {
 
         @Override
         public void addChangeListener(ChangeListener l) {
+            listeners.add(l);
         }
 
         @Override
         public void removeChangeListener(ChangeListener l) {
+            listeners.remove(l);
         }
 
         @Override
         public Node node(FileObject key) {
             try {
                 DataObject dob = DataObject.find(key);
-                return new AndroidNode(dob.getNodeDelegate());
+                return new AndroidNode(dob.getNodeDelegate(), androidDir, project);
             } catch (DataObjectNotFoundException ex) {
                 Exceptions.printStackTrace(ex);
             }
@@ -98,10 +116,23 @@ public class AndroidExplorerFactory implements NodeFactory {
 
     }
 
-    private class AndroidNode extends FilterNode {
+    public class AndroidNode extends FilterNode implements ChangeListener, PropertyChangeListener {
 
-        public AndroidNode(Node original) {
+        final FileObject directory;
+        Project projectGradle;
+        final Project projectAndroid;
+        LogicalViewProvider viewProvider;
+        Node node;
+
+        private final String srcPath;
+
+        public AndroidNode(Node original, FileObject directory, Project projectAndroid) {
             super(original);
+            this.directory = directory;
+            this.projectAndroid = projectAndroid;
+            srcPath = directory.getParent().getPath() + File.separator + "src";
+            DataObject.Registry registries = DataObject.getRegistry();
+            registries.addChangeListener(WeakListeners.change(this, registries));
         }
 
         @Override
@@ -114,7 +145,6 @@ public class AndroidExplorerFactory implements NodeFactory {
             return "Android offline"; //To change body of generated methods, choose Tools | Templates.
         }
 
-
         @Override
         public Image getOpenedIcon(int type) {
             return getIcon(type); //To change body of generated methods, choose Tools | Templates.
@@ -122,10 +152,81 @@ public class AndroidExplorerFactory implements NodeFactory {
 
         @Override
         public Action[] getActions(boolean context) {
-            List<? extends Action> rootActions = Utilities.actionsForPath("Actions/AndroidOfflineNode");
-            return rootActions.toArray(new Action[rootActions.size()]);
+            if (directory.getFileObject("build", "gradle") == null) {
+                List<? extends Action> rootActions = Utilities.actionsForPath("Actions/AndroidOfflineNode");
+                return rootActions.toArray(new Action[rootActions.size()]);
+            } else {
+                if (projectGradle == null) {
+                    try {
+                        projectGradle = ProjectManager.getDefault().findProject(directory);
+                        if (projectGradle == null) { //for first sub project open
+                            NbGradleProjectFactory factory = new NbGradleProjectFactory();
+                            projectGradle = factory.loadProject(directory, new ProjectState() {
+                                @Override
+                                public void markModified() {
+                                }
+
+                                @Override
+                                public void notifyDeleted() throws IllegalStateException {
+                                }
+                            });
+                        }
+                        viewProvider = projectGradle.getLookup().lookup(LogicalViewProvider.class);
+                        node = viewProvider.createLogicalView();
+                    } catch (IOException ex) {
+                        Exceptions.printStackTrace(ex);
+                    } catch (IllegalArgumentException ex) {
+                        Exceptions.printStackTrace(ex);
+                    }
+
+                }
+                return node.getActions(context);
+            }
         }
 
+        private Map<DataObject, Object> registered = new WeakHashMap<>();
+
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            Object source = e.getSource();
+            if ((source instanceof HashSet)) {
+                HashSet<DataObject> dobs = (HashSet<DataObject>) source;
+                for (DataObject next : dobs) {
+                    if (next.getPrimaryFile().getPath().startsWith(srcPath) && !registered.containsKey(next)) {
+                        next.addPropertyChangeListener(WeakListeners.propertyChange(this, next));
+                        registered.put(next, null);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            if (evt.getPropertyName() == null ? DataObject.PROP_MODIFIED == null : evt.getPropertyName().equals(DataObject.PROP_MODIFIED) && ((Boolean) evt.getNewValue()) == false) {
+                DataObject dob = (DataObject) evt.getSource();
+                try {
+                    InputStream is = dob.getPrimaryFile().getInputStream();
+                    FileObject out = FileUtil.createData(new File(directory.getPath() + File.separator + "src" + File.separator + "main" + File.separator + "java" + File.separator + dob.getPrimaryFile().getPath().replaceFirst(srcPath + File.separator, "")));
+                    OutputStream outputStream = out.getOutputStream();
+                    IOUtils.copy(is, outputStream);
+                    is.close();
+                    outputStream.close();
+                    FileUtil.refreshFor(new File(srcPath));
+                } catch (FileNotFoundException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        }
+
+        public Project getProjectAndroid() {
+            return projectAndroid;
+        }
+
+        public FileObject getDirectory() {
+            return directory;
+        }
 
     }
 
